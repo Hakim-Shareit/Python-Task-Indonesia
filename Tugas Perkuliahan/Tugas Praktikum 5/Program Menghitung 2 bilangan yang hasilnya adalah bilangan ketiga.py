bdaa2b8d6f18e5b0e2af87fc20608ba7216d2a0d
Hakim-Shareit <syarifuddinhakim939@gmail.com>
2024-11-17 10:41:17 +0700

    Add files via upload

---
+++ Tugas Perkuliahan/Tugas Praktikum 5/Program Menghitung 2 bilangan yang hasilnya adalah bilangan ketiga.py
@@ -0,0 +1,15 @@
+angka1 = eval(input('Masukkan Angka Pertama = '))
+angka2 = eval(input('Masukkan Angka Kedua = '))
+angka3 = eval(input('Masukkan Angka Ketiga = '))
+
+def cek_trio_ajaib(a, b, c):
+ return (a + b == c) or (a + c == b) or (b + c == a) 
+
+print ('Berikut Adalah hasilnya : ')
+
+if cek_trio_ajaib(angka1, angka2, angka3):
+    print ('True')
+    print(angka1,',',angka2,',',angka3,',',' angka tersebut adalah trio ajaib')
+else :
+    print ('False')
+    print(angka1,',',angka2 ,',',angka3,',','angka tersebut bukanlah trio ajaib')
